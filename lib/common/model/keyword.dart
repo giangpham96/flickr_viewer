a05855b440adc58b37eec6dac539cabd832444d9
Giang Pham <conmeobeo96@gmail.com>
2020-01-10 10:56:34 +0200

    Allow comparing Keyword

--- lib/common/model/keyword.dart
+++ lib/common/model/keyword.dart
@@ -1,6 +1,23 @@
+import 'package:flutter/foundation.dart';
+
 class Keyword {
   final String keyword;
   final List<Keyword> subKeywords;
 
   const Keyword(this.keyword, {this.subKeywords});
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is Keyword &&
+              runtimeType == other.runtimeType &&
+              keyword == other.keyword &&
+              listEquals(subKeywords, other.subKeywords);
+
+  @override
+  int get hashCode =>
+      keyword.hashCode ^
+      subKeywords.hashCode;
+
+
 }
